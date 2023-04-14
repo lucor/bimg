36f2f0c5106b23f955b134cc824a50e5eeebdbd0
Luca Corbo <lu.corbo@gmail.com>
2023-04-14 10:01:00 +0200

    Rgapixel (#3)
    * rgapixels: add custom tile support

---
+++ benchmark/benchmark_test.go
@@ -0,0 +1,35 @@
+package benchmark_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/h2non/bimg"
+)
+
+var images = []struct {
+	source string
+}{
+	{"3x3.jpg"},
+	{"3x3greys.jpg"},
+	{"northern_cardinal_bird.jpg"},
+	{"parameter_trim.png"},
+	{"test.avif"},
+	{"test.gif"},
+	{"test.png"},
+	{"test.webp"},
+	{"test2.heic"},
+	{"test3.heic"},
+	{"transparent.png"},
+}
+
+func Benchmark_RGBAPixels(b *testing.B) {
+	for _, tt := range images {
+		imagefile, _ := os.ReadFile("testdata/" + tt.source)
+		b.Run(tt.source, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				bimg.RGBAPixels(imagefile)
+			}
+		})
+	}
+}
